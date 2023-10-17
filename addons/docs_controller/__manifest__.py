2d4ea8460dbf7054a39c935c39b7b65de2f388bb
Kipras-Zelenkovas <kipraszelenkovas@gmail.com>
2023-10-17 19:11:12 +0300

    Changed that Documents view can see default users.

--- addons/docs_controller/__manifest__.py
+++ addons/docs_controller/__manifest__.py
@@ -24,8 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-       
+        'security/ir.model.access.csv',
         'views/docs.xml'
     ],
     # only loaded in demonstration mode
